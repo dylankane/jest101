41366ceeb5bff36465affb94cac59b0c6024fcfb
Dylan <dylankane3@gmail.com>
2023-05-23 10:44:08 +0000

    Add tests with Jest

---
+++ scripts/tests/calc.test.js
@@ -0,0 +1,28 @@
+/**
+ * @jest-environment jsdom
+ */
+
+const { TestScheduler } = require("jest");
+const addition = require("../calc");
+
+describe("Calculatir", () => {
+    describe("Addition function", () => {
+        test("should return 42 for 20 + 22", () => {
+            expect(addition(20, 22)).toBe(42);
+        });
+        test("should return 73 for 42 + 31", () => {
+            expect(addition(42, 31)).toBe(73);
+        });
+
+    });
+    describe("Subtraction function", () => {
+
+    });
+    describe("Multilication function", () => {
+
+    });
+    describe("Division function", () => {
+
+    });
+
+})
